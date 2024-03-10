1eb5fdef86905b7aa4266c921fb15716abb720b4
Girish Nair <nairgirish100@gmail.com>
2024-03-10 19:45:54 +0400

    added argos

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,18 +8,17 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  workers: process.env.CI ? 2 : undefined,
+
+  reporter: process.env.CI ? [["@argos-ci/playwright/reporter"]] : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+   
+    baseURL: 'http://127.0.0.1:3000',
+    trace: 'retain-on-failure',
+    screenshot: "only-on-failure",
   },
 
   /* Configure projects for major browsers */
